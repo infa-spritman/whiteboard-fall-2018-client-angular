257b68504da87608a2feeeaf07c0d78527457f59
kodefear <pritmani.s@husky.neu.edu>
2018-11-19 03:13:52 -0500

    Corrected Routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -19,9 +19,9 @@ const appRoutes: Routes = [
   { path: 'profile', component: ProfileComponent},
   { path: 'course/:courseId/module', component: CourseViewerComponent },
   { path: 'course/:courseId/section', component: SectionListComponent },
-  { path: 'course/:courseId/module/:moduleId', component: CourseViewerComponent },
-  { path: 'course/:courseId/module/:moduleId/lesson/:lessonId', component: CourseViewerComponent },
-  { path: 'course/:courseId/module/:moduleId/lesson/:lessonId/topic/:topicId', component: CourseViewerComponent },
+  { path: 'course/:courseId/module/:moduleId/lesson', component: CourseViewerComponent },
+  { path: 'course/:courseId/module/:moduleId/lesson/:lessonId/topic', component: CourseViewerComponent },
+  { path: 'course/:courseId/module/:moduleId/lesson/:lessonId/topic/:topicId/widget', component: CourseViewerComponent },
   { path: '**', component: WhiteBoardComponent} // last
 ];
 export const routing = RouterModule.forRoot(appRoutes);
